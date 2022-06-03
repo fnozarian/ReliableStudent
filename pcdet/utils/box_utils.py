868f4d42c581955b9eecb82583345852c85eb577
Farzad <fnozarian@gmail.com>
2022-06-03 16:11:38 +0200

    Bugfix: move gpu tensor to cpu in eval_with_train.

--- pcdet/utils/box_utils.py
+++ pcdet/utils/box_utils.py
@@ -238,6 +238,8 @@ def boxes3d_kitti_camera_to_imageboxes(boxes3d, calib, image_shape=None):
     max_uv = np.max(corners_in_image, axis=1)  # (N, 2)
     boxes2d_image = np.concatenate([min_uv, max_uv], axis=1)
     if image_shape is not None:
+        if isinstance(image_shape, torch.Tensor) and image_shape.is_cuda:
+            image_shape = image_shape.cpu()
         boxes2d_image[:, 0] = np.clip(boxes2d_image[:, 0], a_min=0, a_max=image_shape[1] - 1)
         boxes2d_image[:, 1] = np.clip(boxes2d_image[:, 1], a_min=0, a_max=image_shape[0] - 1)
         boxes2d_image[:, 2] = np.clip(boxes2d_image[:, 2], a_min=0, a_max=image_shape[1] - 1)
