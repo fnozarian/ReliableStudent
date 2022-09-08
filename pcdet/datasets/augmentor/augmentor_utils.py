97ddaa441a94ae5c62e09b0cbc597993a5a8483a
Farzad <fnozarian@gmail.com>
2022-09-08 20:38:21 +0200

    Bugfix: correct checking for all-zero rows in augmentation functions.

--- pcdet/datasets/augmentor/augmentor_utils.py
+++ pcdet/datasets/augmentor/augmentor_utils.py
@@ -96,9 +96,9 @@ def random_flip_along_x_bbox(gt_boxes, enables):
     """
     for i in range(len(enables)):
         if enables[i]:
-            valid_inds = gt_boxes[i].sum(dim=-1).nonzero().squeeze()
-            gt_boxes[i, valid_inds, 1] = -gt_boxes[i, valid_inds, 1]
-            gt_boxes[i, valid_inds, 6] = -gt_boxes[i, valid_inds, 6]
+            valid_mask = torch.all(gt_boxes[i] != 0, dim=-1)
+            gt_boxes[i, valid_mask, 1] = -gt_boxes[i, valid_mask, 1]
+            gt_boxes[i, valid_mask, 6] = -gt_boxes[i, valid_mask, 6]
 
             # if gt_boxes.shape[2] > 7:
             #     gt_boxes[i, :, 8] = -gt_boxes[i, :, 8]
@@ -115,9 +115,9 @@ def random_flip_along_y_bbox(gt_boxes, enables):
     """
     for i in range(len(enables)):
         if enables[i]:
-            valid_inds = gt_boxes[i].sum(dim=-1).nonzero().squeeze()
-            gt_boxes[i, valid_inds, 0] = -gt_boxes[i, valid_inds, 0]
-            gt_boxes[i, valid_inds, 6] = -(gt_boxes[i, valid_inds, 6] + np.pi)
+            valid_mask = torch.all(gt_boxes[i] != 0, dim=-1)
+            gt_boxes[i, valid_mask, 0] = -gt_boxes[i, valid_mask, 0]
+            gt_boxes[i, valid_mask, 6] = -(gt_boxes[i, valid_mask, 6] + np.pi)
 
             # if gt_boxes.shape[2] > 7:
             #     gt_boxes[i, :, 7] = -gt_boxes[i, :, 7]
@@ -135,9 +135,9 @@ def global_rotation_bbox(gt_boxes, rotations):
     """
     for i in range(len(rotations)):
         rotation = rotations[i:i+1]
-        valid_inds = gt_boxes[i].sum(dim=-1).nonzero().squeeze()
-        gt_boxes[i, valid_inds, 0:3] = common_utils.rotate_points_along_z(gt_boxes[i:i+1, valid_inds, 0:3], rotation)[0]
-        gt_boxes[i, valid_inds, 6] += rotation
+        valid_mask = torch.all(gt_boxes[i] != 0, dim=-1)
+        gt_boxes[i, valid_mask, 0:3] = common_utils.rotate_points_along_z(gt_boxes[i:i+1, valid_mask, 0:3], rotation)[0]
+        gt_boxes[i, valid_mask, 6] += rotation
         # if gt_boxes.shape[2] > 7:
         #     gt_boxes[i, :, 7:9] = common_utils.rotate_points_along_z(
         #         np.hstack((gt_boxes[i, :, 7:9], np.zeros((gt_boxes.shape[1], 1))))[np.newaxis, :, :],
@@ -156,8 +156,8 @@ def global_scaling_bbox(gt_boxes, scales):
     Returns:
     """
     for i in range(len(scales)):
-        valid_inds = gt_boxes[i].sum(dim=-1).nonzero().squeeze()
-        gt_boxes[i, valid_inds, :6] *= scales[i]
+        valid_mask = torch.all(gt_boxes[i] != 0, dim=-1)
+        gt_boxes[i, valid_mask, :6] *= scales[i]
     return gt_boxes
 
 
