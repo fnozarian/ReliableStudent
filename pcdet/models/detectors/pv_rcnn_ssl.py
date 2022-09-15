Bugfix: TP, FP, FN calculation and handling empty pred tensors. * Used max over 41 rows of detailed_stats due to accumulation of stats.

pcdet/models/detectors/pv_rcnn_ssl.py
@@ -216,11 +216,11 @@ class PVRCNN_SSL(Detector3DTemplate):
             # NOTE: Need to store them in batch_dict in a new key, which can be removed later
             batch_dict['pseudo_boxes_prefilter'] = torch.zeros_like(batch_dict['gt_boxes'])
             self._fill_with_pseudo_labels(batch_dict, pseudo_boxes, unlabeled_inds, labeled_inds, key='pseudo_boxes_prefilter')
-            
+
             # apply student's augs on teacher's pseudo-boxes (w/o filtered)
             batch_dict = self.apply_augmentation(batch_dict, batch_dict, unlabeled_inds, key='pseudo_boxes_prefilter')
 
-            statistics_prefilter = self.calc_statistics(batch_dict['pseudo_boxes_prefilter'][unlabeled_inds], 
+            statistics_prefilter = self.calc_statistics(batch_dict['pseudo_boxes_prefilter'][unlabeled_inds],
                                                         ori_unlabeled_boxes, True,
                                                         tag='before_filtering',
                                                         pseudo_sem_scores=pseudo_sem_scores)
@@ -353,7 +353,7 @@ class PVRCNN_SSL(Detector3DTemplate):
             class_metrics_all = {}
             class_metrics_batch = {}
             for c, cls_name in enumerate(['Car', 'Pedestrian', 'Cyclist']):
-                metric_value = detailed_stats[c, 0, :, m].sum().item()
+                metric_value = detailed_stats[c, 0, :, m].max().item()
                 class_metrics_all[cls_name] = metric_value
                 class_metrics_batch[cls_name] = metric_value / num_batch
             statistics['all_' + metric_name] = class_metrics_all

pcdet/utils/stats_utils.py
@@ -59,16 +59,18 @@ class KITTIEVAL(Metric):
         for i in range(len(preds)):
             valid_preds_mask = torch.logical_not(torch.all(preds[i] == 0, dim=-1))
             valid_gts_mask = torch.logical_not(torch.all(targets[i] == 0, dim=-1))
-            # TODO(farzad) Check for valid_mast of pred_scores?
+            if pred_scores[i].ndim == 1:
+                pred_scores[i] = pred_scores[i].unsqueeze(dim=-1)
+            valid_pred_scores_mask = torch.logical_not(torch.all(pred_scores[i] == 0, dim=-1))
             valid_pred_boxes = preds[i][valid_preds_mask]
             valid_gt_boxes = targets[i][valid_gts_mask]
-
+            valid_pred_scores = pred_scores[i][valid_pred_scores_mask]
             # Starting class indices from zero
             valid_pred_boxes[:, -1] -= 1
             valid_gt_boxes[:, -1] -= 1
 
             # Adding predicted scores as the last column
-            valid_pred_boxes = torch.cat([valid_pred_boxes, pred_scores[i].unsqueeze(dim=-1)], dim=-1)
+            valid_pred_boxes = torch.cat([valid_pred_boxes, valid_pred_scores], dim=-1)
 
             num_gts = valid_gts_mask.sum()
             num_preds = valid_preds_mask.sum()
@@ -205,9 +207,9 @@ def _prepare_data(gt_annos, dt_annos, current_class):
 
 def clean_data(gt_anno, dt_anno, current_class):
     dc_bboxes, ignored_gt, ignored_dt = [], [], []
-    # TODO(farzad) hardcoded
-    num_gt = gt_anno.numel() // 8  # len(gt_anno["name"])
-    num_dt = dt_anno.numel() // 9  # len(dt_anno["name"])
+
+    num_gt = len(gt_anno)  # len(gt_anno["name"])
+    num_dt = len(dt_anno)  # len(dt_anno["name"])
     num_valid_gt = 0
     # TODO(farzad) cleanup and parallelize
     for i in range(num_gt):