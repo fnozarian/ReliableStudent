Bugfix: changes initialization of precision, recall, etc. from zero to nan and ignores nans in reduce operations (issue #17).

pcdet/models/detectors/pv_rcnn_ssl.py
@@ -353,7 +353,7 @@ class PVRCNN_SSL(Detector3DTemplate):
             class_metrics_all = {}
             class_metrics_batch = {}
             for c, cls_name in enumerate(['Car', 'Pedestrian', 'Cyclist']):
-                metric_value = detailed_stats[c, 0, :, m].max().item()
+                metric_value = np.nanmax(detailed_stats[c, 0, :, m])
                 class_metrics_all[cls_name] = metric_value
                 class_metrics_batch[cls_name] = metric_value / num_batch
             statistics['all_' + metric_name] = class_metrics_all
@@ -370,7 +370,7 @@ class PVRCNN_SSL(Detector3DTemplate):
         # Get calculated recall
         class_metrics_all = {}
         for c, cls_name in enumerate(['Car', 'Pedestrian', 'Cyclist']):
-            class_metrics_all[cls_name] = results['raw_recall'][c].max().item()
+            class_metrics_all[cls_name] = np.nanmax(results['raw_recall'][c])
         statistics['max_recall'] = class_metrics_all
 
         # Draw Precision-Recall curves

pcdet/utils/stats_utils.py
@@ -117,11 +117,11 @@ def eval_class(gt_annos,
     N_SAMPLE_PTS = 41
     num_minoverlap = len(min_overlaps)
     num_class = len(current_classes)
-    precision = np.zeros([num_class, num_minoverlap, N_SAMPLE_PTS])
-    recall = np.zeros([num_class, num_minoverlap, N_SAMPLE_PTS])
-    detailed_stats = np.zeros([num_class, num_minoverlap, N_SAMPLE_PTS, 5])  # TP, FP, FN, Similarity, thresholds
-    raw_precision = np.zeros_like(precision)
-    raw_recall = np.zeros_like(recall)
+    precision = np.nan * np.zeros([num_class, num_minoverlap, N_SAMPLE_PTS])
+    recall = np.nan * np.zeros([num_class, num_minoverlap, N_SAMPLE_PTS])
+    detailed_stats = np.nan * np.zeros([num_class, num_minoverlap, N_SAMPLE_PTS, 5])  # TP, FP, FN, Similarity, thresholds
+    raw_precision = np.nan * np.zeros_like(precision)
+    raw_recall = np.nan * np.zeros_like(recall)
 
     for m, current_class in enumerate(current_classes):
         rets = _prepare_data(gt_annos, dt_annos, current_class)
@@ -166,9 +166,9 @@ def eval_class(gt_annos,
             raw_recall[m, k] = recall[m, k]
             raw_precision[m, k] = precision[m, k]
             for i in range(len(thresholds)):
-                precision[m, k, i] = np.max(
+                precision[m, k, i] = np.nanmax(
                     precision[m, k, i:], axis=-1)
-                recall[m, k, i] = np.max(recall[m, k, i:], axis=-1)
+                recall[m, k, i] = np.nanmax(recall[m, k, i:], axis=-1)
 
     ret_dict = {
         "recall": recall,
@@ -331,17 +331,19 @@ def get_thresholds(scores: np.ndarray, num_gt, num_sample_pts=41):
 
 
 def get_mAP(prec):
-    sums = 0
-    for i in range(0, prec.shape[-1], 4):
-        sums = sums + prec[..., i]
-    return sums / 11 * 100
-
+    # sums = 0
+    # for i in range(0, prec.shape[-1], 4):
+    #     sums = sums + prec[..., i]
+    # return sums / 11 * 100
+    return np.nanmean(prec[..., ::4], axis=-1) * 100
 
 def get_mAP_R40(prec):
-    sums = 0
-    for i in range(1, prec.shape[-1]):
-        sums = sums + prec[..., i]
-    return sums / 40 * 100
+    # sums = 0
+    # for i in range(1, prec.shape[-1]):
+    #     sums = sums + prec[..., i]
+    # return sums / 40 * 100
+    return np.nanmean(prec[..., 1:], axis=-1) * 100
+
 
 
 def _stats(pred_infos, gt_infos):