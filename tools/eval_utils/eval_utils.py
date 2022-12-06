05854bfbf67962404175f8ae4059be9816744aef
Farzad <fnozarian@gmail.com>
2022-12-06 18:20:28 +0100

    Revert "added precision-recall curve during eval using teacher model"
    This reverts commit c3bcae96

--- tools/eval_utils/eval_utils.py
+++ tools/eval_utils/eval_utils.py
@@ -7,9 +7,7 @@ import tqdm
 
 from pcdet.models import load_data_to_gpu
 from pcdet.utils import common_utils
-import copy
-from pcdet.datasets.kitti.kitti_object_eval_python import eval as kitti_eval
-from matplotlib import pyplot as plt
+
 
 def statistics_info(cfg, ret_dict, metric, disp_dict):
     for cur_thresh in cfg.MODEL.POST_PROCESSING.RECALL_THRESH_LIST:
@@ -38,7 +36,6 @@ def eval_one_epoch(cfg, model, dataloader, epoch_id, logger, dist_test=False, sa
     dataset = dataloader.dataset
     class_names = dataset.class_names
     det_annos = []
-    ps_annos = []
 
     logger.info('*************** EPOCH %s EVALUATION *****************' % epoch_id)
     if dist_test:
@@ -57,14 +54,8 @@ def eval_one_epoch(cfg, model, dataloader, epoch_id, logger, dist_test=False, sa
     for i, batch_dict in enumerate(dataloader):
         load_data_to_gpu(batch_dict)
         with torch.no_grad():
-            pred_dicts, ret_dict, tb_dict, pseudo_labels = model(batch_dict)
+            pred_dicts, ret_dict, tb_dict = model(batch_dict)
         disp_dict = {}
-
-        annos = dataset.generate_prediction_dicts(
-            batch_dict, pseudo_labels, class_names,
-            output_path=final_output_dir if save_to_file else None
-        )
-        ps_annos += annos
 
         statistics_info(cfg, ret_dict, metric, disp_dict)
         annos = dataset.generate_prediction_dicts(
@@ -82,7 +73,6 @@ def eval_one_epoch(cfg, model, dataloader, epoch_id, logger, dist_test=False, sa
     if dist_test:
         rank, world_size = common_utils.get_dist_info()
         det_annos = common_utils.merge_results_dist(det_annos, len(dataset), tmpdir=result_dir / 'tmpdir')
-        ps_annos = common_utils.merge_results_dist(ps_annos, len(dataset), tmpdir=result_dir / 'tmpdir')
         metric = common_utils.merge_results_dist([metric], world_size, tmpdir=result_dir / 'tmpdir')
 
     logger.info('*************** Performance of EPOCH %s *****************' % epoch_id)
@@ -122,41 +112,6 @@ def eval_one_epoch(cfg, model, dataloader, epoch_id, logger, dist_test=False, sa
         eval_metric=cfg.MODEL.POST_PROCESSING.EVAL_METRIC,
         output_path=final_output_dir
     )
-
-    eval_gt_annos = [copy.deepcopy(info['annos']) for info in dataset.kitti_infos]
-    # with open(result_dir / 'ps_annos.pkl', 'wb') as f:
-    #     pickle.dump(ps_annos, f)
-    PR_detail_dict = {}
-    ap_result_str, ap_dict = kitti_eval.get_official_eval_result(eval_gt_annos, ps_annos, class_names,
-                                                                 PR_detail_dict=PR_detail_dict)
-
-    detailed_stats_3d = PR_detail_dict['3d_']['detailed_stats']
-    # detailed_stats_3d is a tensor of size [num_class, num_difficulty, num_minoverlap, N_SAMPLE_PTS, NUM_STATS] where
-    # num_class in [0..6], and {0: 'Car', 1: 'Pedestrian', 2: 'Cyclist', 3: 'Van', 4: 'Person_sitting', 5: 'Truck'},
-    # num_difficulty is 3, and {0: 'easy', 1: 'normal', 2: 'hard'},
-    # num_minoverlap is 2, and {0: 'overlap_0_7', 1: 'overlap_0_5'} and overlap_0_7 (for 3D metric),
-    # is [0.7, 0.5, 0.5, 0.7, 0.5, 0.7] for 'Car', 'Pedestrian', etc. correspondingly,
-    # N_SAMPLE_PTS is 41,
-    # NUM_STATS is 5, and {0:'tp', 1:'fp', 2:'fn', 3:'similarity', 4:'precision thresholds'}
-    # for example [0, 1, 0, :, 0] means number of TPs of Car class with normal difficulty and overlap@0.7 for all 41 sample points
-
-    # Reproducing fig. 3 of soft-teacher as an example
-
-    fig, ax1 = plt.subplots()
-    precision = PR_detail_dict['3d_']['precision']
-    recall = PR_detail_dict['3d_']['recall']
-    thresholds = detailed_stats_3d[0, 1, 0, ::-1, -1]
-    prec = precision[0, 1, 0, ::-1]
-    rec = recall[0, 1, 0, ::-1]
-    ax2 = ax1.twinx()
-    valid_mask = ~((rec == 0) | (prec == 0))
-    ax1.plot(thresholds[valid_mask], prec[valid_mask], 'b-')
-    ax2.plot(thresholds[valid_mask], rec[valid_mask], 'r-')
-    ax1.set_xlabel('Foreground score')
-    ax1.set_ylabel('Precision', color='b')
-    ax2.set_ylabel('Recall', color='r')
-
-    plt.savefig(str(result_dir) + '/precision_recall_' + str(epoch_id))
 
     logger.info(result_str)
     ret_dict.update(result_dict)
