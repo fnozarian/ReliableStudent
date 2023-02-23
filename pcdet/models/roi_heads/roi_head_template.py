2b4a6fc6f2cc01f6a2721945043bad462d55ac50
shashankag14 <shashankag14@gmail.com>
2023-02-23 20:13:21 +0100

    DA-35 New weighing type to ignore BG

--- pcdet/models/roi_heads/roi_head_template.py
+++ pcdet/models/roi_heads/roi_head_template.py
@@ -622,6 +622,8 @@ class RoIHeadTemplate(nn.Module):
                 
                 elif self.model_cfg['LOSS_CONFIG']['UL_RCNN_CLS_WEIGHT_TYPE'] == 'ignore_interval':  # Naive baseline
                     unlabeled_rcnn_cls_weights[ul_interval_mask] = 0
+                elif self.model_cfg['LOSS_CONFIG']['UL_RCNN_CLS_WEIGHT_TYPE'] == 'ignore-bg':  # Naive baseline
+                    unlabeled_rcnn_cls_weights[ulb_bg_mask] = 0
                 elif self.model_cfg['LOSS_CONFIG']['UL_RCNN_CLS_WEIGHT_TYPE'] == 'full-ema':
                     unlabeled_rcnn_cls_weights = rcnn_bg_score_teacher[unlabeled_inds]
                 
